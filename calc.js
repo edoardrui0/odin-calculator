f7f62d6b6b8a7bed203b009b05a5ecbca19c7b5c
Edoardo Ruiz <44981900+edoardrui0@users.noreply.github.com>
2024-06-07 14:42:31 -0400

    Basic calc functionality started

--- calc.js
+++ calc.js
@@ -1,3 +1,15 @@
+const calcContainer = document.querySelector("#calc-container");
+const display = document.querySelector("#display");
+display.textContent = "0";
+const buttons = document.querySelector("#buttons");
+
+const clearBtn = document.querySelector("#clear-btn");
+const numberBtn = document.querySelectorAll("#number-btn");
+const operandBtn = document.querySelectorAll("#operand-btn");
+const zeroBtn = document.querySelector("#zero-btn");
+const decimalBtn = document.querySelector("#decimal-btn");
+const equalBtn = document.querySelector("#equal-btn");
+
 // basic logical functions
 let add = (a, b) => a + b;
 let subtract = (a, b) => a - b;
@@ -11,4 +23,29 @@ let numB;
 
 let operate = (numA, numB, operator) => add(numA, numB);
 
-// console.log(operate(5, 8));
+let displayValue = [];
+
+let numClick = () => {
+  numberBtn.forEach((num) => {
+    num.addEventListener("click", () => {
+      displayValue.push(num.value);
+      let joinValue = displayValue.join("");
+      display.textContent = joinValue;
+    });
+  });
+
+  //   zeroBtn.addEventListener("click", () => {
+  //     displayValue = zeroBtn.value;
+  //     display.textContent = displayValue;
+  //   });
+};
+
+let clearClick = () => {
+  clearBtn.addEventListener("click", () => {
+    displayValue = null;
+    display.textContent = "";
+  });
+};
+
+numClick();
+// clearClick();
